a889095096baa166ce10e8775beb4648a828237c
CharlesTogle <149758871+Luhartt@users.noreply.github.com>
2024-11-16 11:07:09 +0800

    Update GroupExercise.java

--- GroupExercise.java
+++ GroupExercise.java
@@ -3,48 +3,6 @@ import java.util.Scanner;
 
 class GroupExercise {
 
-<<<<<<< Updated upstream
-public class Stacks {
-    public int[] givenArray;
-
-    public Stacks(int[] arr) {
-        this.givenArray = arr;
-    }
-}
-
-
-public class Queues {
-
-    private int[] givenArray;
-    private int NumberOfElements = 0;
-    private Boolean EnqueueEnabled = true;
-    private int LeftIndex = 0; //Right pointer (front)
-    private int RightIndex = 0; //left pointer (rear)
-    
-
-    public Queues(int[] arr) {
-        this.givenArray = arr;
-        this.NumberOfElements = arr.length;
-    }
-
-    public int Peek(){
-         return this.LeftIndex  == this.NumberOfElements ? -1 : this.givenArray[this.LeftIndex];
-    } 
-
-    public void Enqueue(int x){
-        if(this.EnqueueEnabled){
-            this.givenArray[this.RightIndex] = x;
-            this.RightIndex++;
-            DisplayElements();
-        }
-
-        if(this.RightIndex == this.NumberOfElements){
-            EnqueueEnabled = false;
-        }
-
-        if (this.isEmpty()){
-            EnqueueEnabled = true;
-=======
     public class Stacks {
         public int[] givenArray;
 
@@ -78,7 +36,6 @@ public class Queues {
             for (int i : givenArray) {
                 System.out.print(i + " ");
             }
->>>>>>> Stashed changes
         }
         
 
